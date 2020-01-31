0a3fbd8a2b6627fae5b32a38e986c4bb2d29179c
Kyle Wong <kylewong2510@gmail.com>
2020-01-30 21:37:31 -0700

    Change extra content

--- main.js
+++ main.js
@@ -5,8 +5,4 @@ closeButton.addEventListener('click', welcomeDisappear)
 
 function welcomeDisappear() {
     closeWelcome.style.display = "none";
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 0afa6c22b3c9b8f987de0a6fadd8a63287f5e99f
