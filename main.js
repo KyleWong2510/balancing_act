125505a6e8e3fee7813af6fffc26b149f5bbd896
Kyle Wong <kylewong2510@gmail.com>
2020-02-01 12:08:29 -0700

    Add function for blue border

--- main.js
+++ main.js
@@ -6,3 +6,30 @@ closeButton.addEventListener('click', welcomeDisappear)
 function welcomeDisappear() {
     closeWelcome.style.display = "none";
 }
+
+//border-left on nav icons//
+var dashboard = document.querySelector('.icon1')
+var transactions = document.querySelector('.icon2')
+var profile = document.querySelector('.icon3')
+
+//add border on wallet and remove from dashboard on click//
+//wallet needs eventlistener
+dashboard.addEventListener('click', addDashboardBorder);
+transactions.addEventListener('click', addTransactionBorder);
+profile.addEventListener('click', addProfileBorder);
+
+function addDashboardBorder() {
+    dashboard.classList.add('blue-border');
+    transactions.classList.remove('blue-border');
+    profile.classList.remove('blue-border');
+}
+function addTransactionBorder() {
+    dashboard.classList.remove('blue-border');
+    transactions.classList.add('blue-border');
+    profile.classList.remove('blue-border');
+}
+function addProfileBorder() {
+    dashboard.classList.remove('blue-border');
+    transactions.classList.remove('blue-border');
+    profile.classList.add('blue-border');
+}
